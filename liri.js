699abfbc5f16d47cd3293a15a2072f1b07fdd5a3
e-holmes <bethholmes768@gmail.com>
2019-10-31 19:21:36 -0700

    Initial commit

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+
+// commands
+// concert0this
+// spotify-this-song
+// movie-this
+// do-what-it-says
