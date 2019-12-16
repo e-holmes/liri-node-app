ab0735bedd3a11714876f6d65149586dba509fcf
e-holmes <bethholmes768@gmail.com>
2019-12-15 18:06:40 -0800

    Updated readme, and completed liri app.

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # liri-node-app
+
+## What is this app?
+This liri-node-app is meant to help you save time in finding out information on your favorite movies and songs. As well as helping you find information on your favorite artists upcoming concert.
+
+## App Index
+    I. Variables
+        a. Variables required for program to run
+            *spotify api
+            *axios
+            *moment
+            *fs
+            *dotenv
+            *keys.js
+            *spotify keys
+        b. Storage Variables
+            *Search Variable Array
+            *Search term place holder
+            *Turning Array into string and storing in search term holder
+            *Function to capitalize first letter of each word in search term
+            *Storage for command
+    II. Command Trigger
+        a. movie-this
+            *Search for information on a movie. Default fill "Mr. Nobody" if no search term given.
+        b. spotify-this-song
+            *Search for information on a song. Default fill "The Sign" if no search term given.
+        c. concert-this
+            *Search for information on an artist next upcoming concert. Default fill "Celion Dion" if no search term given.
+        d. do-what-it-says
+            *Read the Random.txt file and run the command with provided search term.
+        e. undefined
+            *If no command provided prompt user with message asking for command with list of available commands.
+    III. Functions
+        a. getMovie function for movie-this feature
+            *Autofills "Mr. Nobody" if no search term provided. Does API request with search term and returns movie title, release date, IMDB rating, Rotten Tomato score, country of origin, languages available, movie plot, and actors.
+        b. getSong function for spotify-this-song feature
+            *Autofills "The Sign" if no search term provided. Does API request with search term and returns song name, album name, artist(s), link to spotify preview.
+        c. getConcert function for concert-this feature
+            *Autofills "Celion Dion" if no search term provided. Does API request with search term and returns name of concert venue, location of concert, and date of concert.
+        d. getRandom function for do-what-it-says feauture
+            *Reads random.txt file and creates and array to store the contents. Fetches the first item in the array and ids it as a spotify-this-song command. Trims the " off the beginning and end of search term from the second array item and stores it in search term variable holder. Runs the capitalize function to capitalize the first letter of each search term. Then runs getSong function.
+        e. capitalize function
+            *Turns the string into an array. Steps through array capitalizing the first letter of each word in the array. Stitches array back into string.
+
+
+## How To use This App
+    Step 1. In git command console type "npm install" and hit enter
+
+    Step 2. Decide wether you want to find out information on a movie, song, artists upcoming concert, or want to see what random.txt does.
+
+    Step 3. Type "node liri.js" and then type the command for which feature you want:
+        a. movie-this (MOVIE NAME)
+        b. spotify-this-song (SONG NAME)
+        c. concert-this (ARTIST NAME)
+        d. do-what-it-says
+    
+    Step 4. Hit enter
+
+    https://drive.google.com/file/d/1ifSkCKXkr4rIFHbua10oq8OoFYLQ1SpZ/view?usp=sharing - automatic!
+    [Google](https://drive.google.com/file/d/1ifSkCKXkr4rIFHbua10oq8OoFYLQ1SpZ/view?usp=sharing)
+
+
+## Technologies Included
+* Node-Spotify-API
+    * https://www.npmjs.com/package/node-spotify-api
+
+* Axios
+    * https://www.npmjs.com/package/axios
+
+    * OMDB API
+        * http://www.omdbapi.com/
+
+    * Bands In Town API
+        * http://www.artists.bandsintown.com/bandsintown-api
+
+* Moment
+    * https://www.npmjs.com/package/moment
+
+* DotEnv
+    * https://www.npmjs.com/package/dotenv
+
+
+
+## Developer
+    Elizabeth Holmes was the sole developer of this app and provides all maintenance. To report any bugs email her at apples.banana@oranges.com.
--- liri.js
+++ liri.js
@@ -1,10 +1,154 @@
+
+// Required run variables
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require('moment');
+var fs = require("fs");
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 
-// commands
-// concert0this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+// Storage Variables
+var nodeArgs = process.argv;
+var value = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 3 && i < nodeArgs.length) {
+    value = value + " " + nodeArgs[i];
+  } else {
+    value += nodeArgs[i];
+  }
+}
+// Capitalizes first letter of search term
+capitalize();
+
+// Store Command
+var command = process.argv[2];
+
+
+// ID which command has been input and run proper function
+switch (command){
+  case "movie-this":
+    getMovie();
+    break;
+  case "spotify-this-song":
+    getSong();
+    break;
+  case "concert-this":
+    getConcert();
+    break;
+  case "do-what-it-says":
+    getRandom();
+    break;
+  case undefined:
+    console.log("Oops! You need to tell me a command and search term. Command options are 'movie-this', 'spotify-this-song', 'concert-this', and 'do-what-it-says'. If you don't provide a search term one will autofill.")
+} 
+
+// movie-this feature
+function getMovie(){
+  if (value === ""){
+    value = "Mr.Nobody";
+  }
+
+  axios.get("http://www.omdbapi.com/?t="+value +"&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+      var title = response.data.Title;
+      var release= response.data.Released;
+      var rate= response.data.imdbRating;
+      var review= response.data.Ratings[1].Value;
+      var origin = response.data.Country;
+      var language = response.data.Language;
+      var plot =  response.data.Plot;
+      var actors =  response.data.Actors;
+
+      console.log("Title: " +title);
+      console.log("Release Date: " +release);
+      console.log("IMDB Rating: " +rate);
+      console.log("Rotten Tomato Score: " +review);
+      console.log("Country of Origin: " +origin);
+      console.log("Languages Available: " +language);
+      console.log("Movie Plot: " +plot);
+      console.log("Actors: " +actors);
+    }
+  );
+}
+
+// spotify-this-song feature
+function getSong(){
+  if (value === ""){
+    value = "The Sign";
+    }
+    console.log(value);
+  spotify.search({ type: 'track', query: value, limit: '10' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    for (i=0; i<10; i++){
+      if (data.tracks.items[i].name == value){
+        var name = data.tracks.items[i].name;
+        var album = data.tracks.items[i].album.name;
+        var artist = data.tracks.items[i].album.artists[0].name;
+        var link = data.tracks.items[i].external_urls.spotify;
+        console.log("Song: " +name); 
+        console.log("Album: " +album); 
+        console.log("Artist(s): " +artist);
+        console.log("Link: " +link);
+        break;
+      }
+    }
+  }); 
+}
+
+
+  //concert-this feaatures
+  function getConcert(){
+    if (value === ""){
+      value = "Celion Dion";
+    }
+    axios.get("https://rest.bandsintown.com/artists/" +value +"/events?app_id=codingbootcamp").then(
+    function(response) {
+      if (response.data === null){
+        return console.log(error);
+        } else {
+          var name = response.data[0].venue.name;
+          var location = response.data[0].venue.city +", " +response.data[0].venue.region +", " +response.data[0].venue.country;
+          var date = response.data[0].datetime;
+
+          date = moment(date, "YYYY-MM-DD THH:mm:ss").format("MM-DD-YYYY");    
+          console.log("Name of Venue: " +name);
+          console.log("Location of Venue: " +location);
+          console.log("Date of Concert: " +date); 
+    }
+      }
+      )
+  }
+  
+
+  // do-what-it-says Command Feature
+  function getRandom(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error) {
+        return console.log(error);
+      }
+      var dataArr = data.split(",");
+     if (dataArr[0] === "spotify-this-song"){
+       value = dataArr[1].substring(1, 19);
+       capitalize();
+       getSong();
+     }
+    
+    });
+  }
+
+
+  function capitalize(){
+    value = value.split(" ");
+    // capitalize each word
+    for (i = 0; i < value.length; i++)
+    {
+      value[i] = value[i].substring(0, 1).toUpperCase() + value[i].substring(1).toLowerCase();
+    }
+    // rejoin back into a sentence
+    value = value.join(" ", value);
+  }  
